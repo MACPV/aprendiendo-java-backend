042b2c014ad18bd79265a972d9a2936d8e2955d7
macpv <aldo.pintado.98@gmail.com>
2024-06-26 02:44:52 -0500

    Ejercicio de "Hola Mundo"

---
+++ Ejercicios/HolaMundo.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+public class HolaMundo {
+    public static void main(String[] args) {
+        String minombre = "Aldo";
+        int miedad = 25;
+        System.out.println("Bienvenido a tu curso de Java");
+        System.out.println("Mi nombre es: " + minombre);
+        System.out.println("Tengo: " + miedad + " años");
+}
+}
