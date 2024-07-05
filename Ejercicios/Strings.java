f32e6b98163dada475ff98ac2bc8dd157dd040ae
macpv <aldo.pintado.98@gmail.com>
2024-07-04 20:18:30 -0500

    String ejercicios 1

--- Ejercicios/Strings.java
+++ Ejercicios/Strings.java
@@ -10,6 +10,26 @@ public class Strings {
         String cadena = t.nextLine();
         System.out.println("La longitud de la cadena es: " + cadena.length());
 
-        
+        //Concatenación de Cadenas
+        System.out.println("Ingresa tu nombre: ");
+        String nombre = t.nextLine();
+        System.out.println("Ingresa tu apellido: ");
+        String apellido = t.nextLine();
+        System.out.println("Hola " + nombre.concat(" ").concat(apellido));
+
+        //Extracción de Subcadena
+        System.out.println("Ingresa una cadena: ");
+        String cad = t.nextLine();
+        System.out.println("Ingresa el primer indice: ");
+        int i1 = t.nextInt() - 1;
+        System.out.println("Ingresa el segundo indice: ");
+        int i2 = t.nextInt() - 1;
+
+        try {
+            System.out.println("La cadena resultado es: " + cad.substring(i1, i2));
+        } catch (Exception e){
+            System.out.println("Debes ingresar un valor entre 1 y " + cad.length());
+        }
+
     }
 }
